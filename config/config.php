e8fd9003db328c8b903d534a89f2efe685ca537e
Sirichat Pannoi <sirichat2003@gmail.com>
2024-09-13 15:51:45 +0700

    add URL

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-    $siteUrl = "http://localhost/hack";
-    $siteUrlImage = "http://localhost/hack";
+    $siteUrl = "http://localhost:8888/my_php_project/";
+    $siteUrlImage = "http://localhost:8888/my_php_project/";
     
     // $line_notify_token = "t18Sc2qpCuJ9gGXxDEaJ7eHdFBgJKuj2f9o8cNe9qTB";
     // $mailTo = "dtt@nu.ac.th";
